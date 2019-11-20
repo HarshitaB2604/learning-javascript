0b6a639c9cf7d8aab2a4514988f978a9a64097d3
HarshitaB2604 <57848574+HarshitaB2604@users.noreply.github.com>
2019-11-20 14:50:56 -0800

    Create variable daily activity.js

---
+++ variable daily activity.js
@@ -0,0 +1,18 @@
+function start()
+{
+	var partOfDay = "morning";
+	var time = 7;
+	
+	println("In the " + partOfDay + ", I wake up at " + time);
+	
+	var partOfDay = "afternoon";
+	var time = 12;
+	
+	println("In the " + partOfDay + ", I eat lunch at " + time);
+	
+	var partOfDay = "evening";
+	var time = 11;
+	
+	println("In the " + partOfDay + ", I go to bed at " + time);
+	
+}
