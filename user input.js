d9f8b1820dad282b70684c0fc1673cd576fe2bf3
HarshitaB2604 <57848574+HarshitaB2604@users.noreply.github.com>
2019-11-20 14:55:53 -0800

    Create user input.js

---
+++ user input.js
@@ -0,0 +1,8 @@
+function start()
+{
+    var  name = readLine("What is your name?");
+    var time = readInt("What time should we meet for dinner?");
+    
+    println("Hi " + name + ", I will meet you at " + time + " o'clock");
+	
+}
