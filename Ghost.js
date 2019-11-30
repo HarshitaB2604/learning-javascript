ccfcd890043800a7b042f6e7c16cfaa851780fef
HarshitaB2604 <57848574+HarshitaB2604@users.noreply.github.com>
2019-11-30 14:08:45 -0800

    Create Ghost.js
    Draws a ghost on a canvas

---
+++ Ghost.js
@@ -0,0 +1,46 @@
+// Constants for main ghost body
+var HEAD_RADIUS = 70;
+var BODY_WIDTH = HEAD_RADIUS * 2;
+var BODY_HEIGHT = 120;
+var NUM_FEET = 3;
+var FOOT_RADIUS = (BODY_WIDTH) / (NUM_FEET * 2);
+var BODY_COLOR = Color.red;
+
+// Constants for eyes
+var PUPIL_RADIUS = 8;
+var PUPIL_LEFT_OFFSET = 16;
+var PUPIL_RIGHT_OFFSET = 40;
+var EYE_RADIUS = 20;
+var EYE_OFFSET = 28;
+var EYE_COLOR = Color.white;
+var PUPIL_COLOR = Color.blue;
+
+/* This program draws a ghost on the canvas. */
+function start()
+{
+    //draws the head of the ghost
+    var head = new Circle(HEAD_RADIUS);
+    head.setPosition(getWidth()/2, getHeight()/2 - HEAD_RADIUS);
+    head.setColor(BODY_COLOR);
+    add(head);
+    
+    
+    //draws body of the ghost
+    var body = new Rectangle(BODY_WIDTH, BODY_HEIGHT);
+    body.setPosition(getWidth()/2 - 
+    HEAD_RADIUS, getHeight()/2 - HEAD_RADIUS);
+    body.setColor(BODY_COLOR);
+    add(body);
+
+    //draws the legs of the body
+    var legPX = (getWidth()/2 - HEAD_RADIUS) + FOOT_RADIUS;
+    for(var x = 0; x < 3; x++)
+    {
+        var leg = new Circle(FOOT_RADIUS);
+        leg.setPosition(legPX, getHeight() - (BODY_HEIGHT + HEAD_RADIUS));
+        leg.setColor(BODY_COLOR);
+        add(leg);
+        
+        legPX = legPX + FOOT_RADIUS*2;
+    }
+}
